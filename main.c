ea265d634c7b1ece97ecca0d1da6488f4b556aa8
luozguo <anitadou9@gmail.com>
2025-12-25 16:06:31 +0100

    minimum program

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 int main(int argc , char**argv)
 {
     int size;
-
-    // parse -> returned the parsed int * with size
-
-    // build stack
     t_stack a;
     t_stack b;
+    int*int_list;
+    // parse -> returned the parsed int * with size
+    int_list = parse_integer_input(argc, argv , &size);
+    normalize_values(int_list , size);
+    
+    // build stack
+    stack_builder(&a , int_list, size, 'a');
+    stack_builder(&b , NULL, 0, 'b');
+
+    dispatch_sort(&a , &b);
     
     return 0;
 
